5db0491d37afc879305977a12ee4b4d63293d194
Kolawole <omotoshok9@gmail.com>
2023-08-05 16:11:33 +0100

    added footer and header modules

--- src/app/@components/website/website.module.ts
+++ src/app/@components/website/website.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { WebsiteComponent } from './website.component';
 import { FormsModule } from '@angular/forms';
 import { WebsiteRoutingModule } from './website-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [WebsiteComponent],
+  declarations: [WebsiteComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule, FormsModule, WebsiteRoutingModule],
 })
 export class WebsiteModule {}
